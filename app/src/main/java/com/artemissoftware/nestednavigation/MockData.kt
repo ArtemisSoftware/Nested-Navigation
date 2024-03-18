bbce74a476f32b100d7e196aeb78bc4fa5497662
gustavo <gustavo.isaac@gmail.com>
2024-03-18 05:40:26 +0000

    MockData

---
+++ app/src/main/java/com/artemissoftware/nestednavigation/MockData.kt
@@ -0,0 +1,112 @@
+package com.artemissoftware.nestednavigation
+
+import com.artemissoftware.nestednavigation.models.Gallery
+
+object MockData {
+
+    val galleries = listOf(
+        Gallery(
+            1,
+            "Fresh Vegges and Greens",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food1,
+        ),
+        Gallery(
+            2,
+            "Best blue berries",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food2,
+        ),
+        Gallery(
+            3,
+            "Cherries La Bloom",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food3,
+        ),
+        Gallery(
+            4,
+            "Fruits everyday",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food4,
+        ),
+        Gallery(
+            5,
+            "Sweet and sour",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food5,
+        ),
+        Gallery(
+            6,
+            "Pancakes for everyone",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food6,
+        ),
+        Gallery(
+            7,
+            "Cupcakes and sparkle",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food7,
+        ),
+        Gallery(
+            8,
+            "Best Burgers shop",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food8,
+        ),
+        Gallery(
+            9,
+            "Coffee of India",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food9,
+        ),
+        Gallery(
+            10,
+            "Pizza boy town",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food10,
+        ),
+        Gallery(
+            11,
+            "Burgers and Chips",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food11,
+        ),
+        Gallery(
+            12,
+            "Breads and butter",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food12,
+        ),
+        Gallery(
+            13,
+            "Cupcake factory",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food13,
+        ),
+        Gallery(
+            14,
+            "Breakfast paradise",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food14,
+        ),
+        Gallery(
+            15,
+            "Cake and Bake",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food15,
+        ),
+        Gallery(
+            16,
+            "Brunch and Stakes",
+            "Very awesome list item has very awesome subtitle. This is bit long",
+            R.drawable.food16,
+        ),
+    )
+
+    val item = Gallery(
+        1,
+        "Awesome List Item",
+        "Very awesome list item has very awesome subtitle. This is bit long",
+        R.drawable.food6,
+    )
+}
